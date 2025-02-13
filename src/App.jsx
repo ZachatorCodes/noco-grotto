da1cb6e50c4b040b7bddc6414f9da3f7160ad20e
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-13 16:55:16 -0700

    Change routes to include about component and no base name

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router'
-import Home from './Home'
-import Navbar from './Navbar'
+import { Routes, Route } from "react-router";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import About from "./About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="" element={<Home />} />
+        <Route exact path="about" element={<About />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
