fb0c1bd966db376e25f4ee52841b9b0cb40a9d8d
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-13 16:56:03 -0700

    Prepend basename with slash

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="noco-grotto">
+    <BrowserRouter basename="/noco-grotto">
       <App />
     </BrowserRouter>
   </StrictMode>
