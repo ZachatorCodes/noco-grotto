a48edf59af3cbfb85eeaf485393bf7537d65ca7e
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-13 17:29:04 -0700

    Convert router type and remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as Router} from "react-router";
+import { HashRouter as Router} from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router basename="noco-grotto">
+    <Router>
       <App />
     </Router>
   </StrictMode>
