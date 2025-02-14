79e26d48a602472b0804a20aeabdefd98069e49a
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-14 02:15:40 -0700

    Change formatting for homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,19 +10,19 @@ function Home() {
       }}
     >
       <div className="hero-overlay"></div>
-      <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-md">
-          <div className="flex justify-center items-center">
-            <img src={logo} className="size-75 mr-5" />
-            <h1 className="mb-5 text-7xl font-bold">
+      <div className="hero-content text-center text-white">
+        <div className="max-w-lg">
+          <div className="flex justify-center items-center mb-5">
+            <img src={logo} className="size-80 mr-5" />
+            <h1 className="mb-5 text-8xl font-bold text-le">
               Northern Colorado Grotto
             </h1>
           </div>
-          <p className="mb-5 text-2xl">
+          <p className="mb-5 text-3xl">
             Promoting responsible access, exploration, research, and
             conservation of caves in Colorado.
           </p>
-          <button className="btn btn-primary">Learn More</button>
+          <button className="btn btn-secondary">Learn More</button>
         </div>
       </div>
     </div>
