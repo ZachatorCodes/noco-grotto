ef01888ddb2eeef50950d23a2ed445a82096aab7
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-14 02:07:03 -0700

    Create new basic landing page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,28 @@
 import cavebg from "./assets/cave-bg-2.jpg";
+import logo from "./assets/logo512.png";
 
 function Home() {
   return (
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage:
-          `url(${cavebg})`,
+        backgroundImage: `url(${cavebg})`,
       }}
     >
       <div className="hero-overlay"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <div className="flex justify-center items-center">
+            <img src={logo} className="size-75 mr-5" />
+            <h1 className="mb-5 text-7xl font-bold">
+              Northern Colorado Grotto
+            </h1>
+          </div>
+          <p className="mb-5 text-2xl">
+            Promoting responsible access, exploration, research, and
+            conservation of caves in Colorado.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary">Learn More</button>
         </div>
       </div>
     </div>
