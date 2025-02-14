9f781c39a152a1c395eb2ae6382e0292d4933f6c
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-13 17:42:58 -0700

    Change back to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter as Router} from "react-router";
+import { BrowserRouter as Router} from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
+    <Router basename="/noco-grotto">
       <App />
     </Router>
   </StrictMode>
