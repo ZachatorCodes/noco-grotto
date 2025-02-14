bd8c145c162428af4a342b83caffa919d273ffb7
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-13 21:28:07 -0700

    Create basic footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 absolute bottom-0">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - Created by Zach Schwartz for The Northern Colorado Grotto
+        </p>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer;
