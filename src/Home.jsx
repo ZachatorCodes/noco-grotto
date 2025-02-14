9671ac7760034c8f8ce6092bc612acd044d53394
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-14 02:30:01 -0700

    Make hero content size responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,14 +11,14 @@ function Home() {
     >
       <div className="hero-overlay"></div>
       <div className="hero-content text-center text-white">
-        <div className="max-w-lg">
-          <div className="flex justify-center items-center mb-5">
-            <img src={logo} className="size-80 mr-5" />
-            <h1 className="mb-5 text-8xl font-bold">
+        <div className="max-w-sm lg:max-w-lg">
+          <div className="flex justify-center items-center mb-1 xl:mb-5">
+            <img src={logo} className="size-40 lg:size-60 xl:size-80 xl:mr-5" />
+            <h1 className="text-4xl lg:text-6xl xl:text-8xl font-bold">
               Northern Colorado Grotto
             </h1>
           </div>
-          <p className="mb-5 text-3xl">
+          <p className="mb-5 text-lg lg:text-xl xl:text-2xl">
             Promoting responsible access, exploration, research, and
             conservation of caves in Colorado.
           </p>
