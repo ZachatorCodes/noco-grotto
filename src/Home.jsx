0f183db3ef12ef25e98b3dae8126c7e30933f160
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-14 01:00:23 -0700

    Move image out of header

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,6 @@ function Home() {
           "flex flex-col items-center bg-cover bg-center w-full h-auto p-5"
         }
       >
-        <img src={logo} className="size-45 lg:size-75 mb-1 lg:mb-3" />
         <div className="bg-accent rounded-box text-center p-3 lg:p-10">
           <h1 className="text-3xl lg:text-5xl text-accent-content font-bold lg:mb-5">
             Northern Colorado Grotto
@@ -21,6 +20,7 @@ function Home() {
           </p>
         </div>
       </div>
+      {/* <img src={logo} className="size-45 lg:size-75 mb-1 lg:mb-3" /> */}
     </>
   );
 }
