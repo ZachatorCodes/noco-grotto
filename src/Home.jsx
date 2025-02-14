ad150025920e295cb5f30cefdc9aff1020342a35
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-14 00:52:26 -0700

    Add margin to bottom of image for spacing

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
           "flex flex-col items-center bg-cover bg-center w-full h-auto p-5"
         }
       >
-        <img src={logo} className="size-45 lg:size-75" />
+        <img src={logo} className="size-45 lg:size-75 mb-1 lg:mb-3" />
         <div className="bg-accent rounded-box text-center p-3 lg:p-10">
           <h1 className="text-3xl lg:text-5xl text-accent-content font-bold">
             Northern Colorado Grotto
