8e80585a33cc1d9a617bcd3021fed11c59d258f0
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-14 01:50:17 -0700

    Basic Hero class component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,27 @@
-import logo from "./assets/logo512.png";
-import cavebg from "./assets/cave-bg-1.jpg";
+import cavebg from "./assets/cave-bg-2.jpg";
 
 function Home() {
   return (
-    <>
-      <div
-        style={{ backgroundImage: `url(${cavebg})` }}
-        className={
-          "flex flex-col items-center bg-cover bg-center w-full h-auto p-5"
-        }
-      >
-        <div className="bg-accent rounded-box text-center p-3 lg:p-10">
-          <h1 className="text-3xl lg:text-5xl text-accent-content font-bold lg:mb-5">
-            Northern Colorado Grotto
-          </h1>
-          <p className="text-m lg:text-xl text-accent-content">
-            Promoting responsible access, exploration, research, and
-            conservation of caves in Colorado.
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          `url(${cavebg})`,
+      }}
+    >
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
           </p>
+          <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
-      {/* <img src={logo} className="size-45 lg:size-75 mb-1 lg:mb-3" /> */}
-    </>
+    </div>
   );
 }
 
