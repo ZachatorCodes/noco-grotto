199461b3913d555d4dd733ab067ec535f68644dd
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-13 19:10:29 -0700

    Add Route to Membership

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import About from "./About";
+import Membership from "./Membership";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/membership" element={<Membership />} />
         <Route path="/*" element={<Navigate replace to="/" />} />
       </Routes>
     </>
