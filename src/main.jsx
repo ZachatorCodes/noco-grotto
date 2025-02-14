e0089a326cb8a5113f4d9d845f88b68088636bc9
Zach Schwartz <zachatorcodes@gmail.com>
2025-02-13 17:09:59 -0700

    Change browser router to hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter as Router} from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="noco-grotto">
+    <Router basename="noco-grotto">
       <App />
-    </BrowserRouter>
+    </Router>
   </StrictMode>
 );
