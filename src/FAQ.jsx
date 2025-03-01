f259240f0b6256eacdbe958fa6bec738cb63fa65
Zach Schwartz <zachatorcodes@gmail.com>
2025-03-01 15:15:47 -0700

    Add header to FAQ page

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -1,9 +1,21 @@
+import cavebg from "/cave-bg-1.jpg";
 function FAQ() {
   return (
-    <div>
-      <h1>FAQ</h1>
-    </div>
-  )
+    <>
+      <div
+        style={{ backgroundImage: `url(${cavebg})` }}
+        className={
+          "flex flex-col items-center bg-cover bg-center w-full h-auto p-5"
+        }
+      >
+        <div className="bg-accent rounded-box text-center p-3 lg:p-10">
+          <h1 className="text-3xl lg:text-5xl text-accent-content font-bold">
+            Frequently Asked Questions
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default FAQ
+export default FAQ;
